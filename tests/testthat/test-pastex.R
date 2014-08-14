da157378af8a07b4b9f2cfa30afff5606900df13
trinker <tyler.rinker@gmail.com>
2014-08-13 20:28:47 -0400

    adde unit test pastex

---
+++ tests/testthat/test-pastex.R
@@ -0,0 +1,16 @@
+context("Checking pastex")
+
+test_that("pastex gets strings and pastes together",{
+
+	expect_identical(pastex("X", "Y", "Z"), "X|Y|Z")
+    expect_identical(pastex("@rm_percent", "@rm_dollar"),
+    	"\\(?[0-9.]+\\)?%|\\$\\(?[0-9.]+\\)?")
+    expect_identical(pastex("@rm_percent", "@@time_12_hours"),
+    	 "\\(?[0-9.]+\\)?%|(1[012]|[1-9]):[0-5][0-9](\\s?)(am|pm)")
+	
+})
+
+
+
+
+
