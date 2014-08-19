eaf70f5728dcf4d93107df6e7c15f023aa394f70
trinker <tyler.rinker@gmail.com>
2014-08-18 22:37:26 -0400

    added rm_endmark testing

---
+++ tests/testthat/test-rm_endmark.R
@@ -0,0 +1,27 @@
+context("Checking rm_endmark")
+
+test_that("rm_endmark is removing/replacing emoticon strings",{
+
+    x <- c("I like the dog.", "I want it *|", "I;", 
+        "Who is| that?", "Hello world", "You...")
+
+	x2 <- c("I like the dog", "I want it", "I;", "Who is| that", "Hello world", 
+		"You")
+
+    expect_true(identical(rm_endmark(x), x2))
+
+})
+
+
+test_that("rm_endmark is extracting emoticon strings",{
+
+    x <- c("I like the dog.", "I want it *|", "I;", 
+        "Who is| that?", "Hello world", "You...")
+
+    x3 <- list(".", "*|", NA_character_, "?", NA_character_, "...")
+
+    expect_true(identical(rm_endmark(x, extract=TRUE), x3))
+})
+
+
+
