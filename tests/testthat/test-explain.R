4e3bd81f40d4aeb94d5f7f95029bc325aab406dd
Tyler <tyler.rinker@gmail.com>
2014-12-28 10:59:06 -0500

    added test for explain

---
+++ tests/testthat/test-explain.R
@@ -0,0 +1,11 @@
+context("Checking explain")
+
+test_that("explain produces a character vector",{
+
+    m <- explain("(?<=foo)\\s+\\(\\d{4}\\)")
+    expect_true(is.character(m))
+    expect_true(length(as.character(m)) > 8)
+    
+})
+
+
