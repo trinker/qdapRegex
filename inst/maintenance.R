82f3d185dcb7132b9d341523fff0abe218ac76bb
Tyler <tyler.rinker@gmail.com>
2015-04-04 22:27:06 -0400

    upped verson for CRAN re-submission

--- inst/maintenance.R
+++ inst/maintenance.R
@@ -27,9 +27,11 @@ update_version("0.3.0")
 #staticdocs dev version
 #========================
 #packages
-# library(devtools); install_github("staticdocs", "hadley")
-# install_github("acc.roxygen2", "trinker");install.packages("rstudioapi")
-library(highlight);  library(staticdocs); 
+pacman::p_load_gh("hadley/staticdocs")
+pacman::p_load(highlight)
+if (!pacman::p_exists(acc.roxygen2, TRUE)) {
+    pacman::p_install_gh("trinker/acc.roxygen2")
+}
 
 #STEP 1: create static doc  
 #right now examples are FALSE in the future this will be true
@@ -51,13 +53,14 @@ R_USER <-  switch(Sys.info()[["user"]],
 path <- "inst/web"
 path2 <- file.path(path, "/index.html")
 rdme <- "inst/extra_statdoc/readme.R"
-library(acc.roxygen2); library(qdap);
-extras <- qcv("%|%", "rm_twitter_url", "rm_white_bracket", "rm_white_colon", 
+
+
+extras <- qdap::qcv("%|%", "rm_twitter_url", "rm_white_bracket", "rm_white_colon", 
 	"rm_white_comma", "rm_white_endmark", "rm_white_lead", "rm_white_trail", 
 	"rm_white_lead_trail", "rm_white_multiple", "rm_white_punctuation", "L", 
 	"U")
 ## drops <- qcv()
-expand_statdoc(path2, to.icon = extras, readme = rdme)
+acc.roxygen2::expand_statdoc(path2, to.icon = extras, readme = rdme)
 
 x <- readLines(path2)
 x[grepl("<h2>Authors</h2>", x)] <- paste(c("<h2>Author</h2>", 
@@ -117,7 +120,7 @@ cat(paste(x, collapse="\n"), file=path2)
 #STEP 3: move to trinker.guthub
 library(reports)
 file <- file.path(R_USER, "GitHub/trinker.github.com/")
-#incoming <- file.path(file, "qdapRegex")
+# incoming <- file.path(file, "qdapRegex")
 delete(incoming)
 file.copy(path, file, TRUE, TRUE)
 file.rename(file.path(file, "web"), incoming)
