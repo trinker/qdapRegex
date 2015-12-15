3f6d365c9057de86b496a0e2c9d9ab748cda46e7
Tyler <tyler.rinker@gmail.com>
2015-12-14 22:38:16 -0500

    added test for ex_

--- tests/testthat/test-rm_.R
+++ tests/testthat/test-rm_.R
@@ -18,3 +18,24 @@ test_that("rm_ is making a function, and operating as expected",{
     	list(c("12", "34", "56", "78")))
 
 })
+
+test_that("ex_ is making a function, and operating as expected",{
+	
+	x <- structure(list(c("12", "34", "56", "78")), class = c("extracted", "list"))
+	
+    ex_digit <- ex_(pattern="[0-9]+")
+    expect_equivalent(ex_digit("  I 12 li34ke ice56cream78.  "), x)
+    
+    ex_lead <- ex_(pattern="^\\s+", trim = FALSE, clean = FALSE)
+    expect_equivalent(ex_lead("  I 12 li34ke ice56cream78.    "), 
+        structure(list("  "), class = c("extracted", "list")))
+    
+    ex_all_except_letters <- ex_(pattern="[^ a-zA-Z]+")
+    expect_equivalent(ex_all_except_letters("  I 12 li34ke ice56cream78.    "), 
+        structure(list(c("12", "34", "56", "78.")), class = c("extracted", "list")))
+    
+    extract_consec_num <- ex_(pattern="[0-9]+", extract = FALSE)
+    expect_equivalent(extract_consec_num("  I 12 li34ke ice56cream78.  "),
+    	"I like icecream.")
+
+})
